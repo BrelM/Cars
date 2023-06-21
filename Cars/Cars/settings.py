3007066c58dec627b3b1de24cbe8ccb63e88b1d4
junssashu2 <sasha@facsciences-uy1.cm>
2023-06-21 15:09:30 +0100

    adding CRUD (for React Native) Users

--- Cars/Cars/settings.py
+++ Cars/Cars/settings.py
@@ -37,13 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-    'visitor.app.VisitorConfig',
-    'user.app.UserConfig',
-=======
-    #'user.apps.UserConfig'
-    #'visitor.apps.VisitorConfig'
->>>>>>> ef587fdcd4360656334974be91614ed473685987
+    'visitor.apps.VisitorConfig',
+    'user.apps.UserConfig',
+    'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ORIGIN_WHITELIST = [
+     'https://127.0.0.1:8000'
+]
